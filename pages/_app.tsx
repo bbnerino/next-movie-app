93a85829e8423744944e16496faa475c34b78cb0
임현홍 <bbnerino@gmail.com>
2022-11-30 22:38:03 +0900

    styled component 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { GlobalStyle } from "../styles/global-style";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MOVIE APP</title>
+        <meta name="description" content="next와 함께하는 movie-app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
